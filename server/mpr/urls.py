6e1ff58b2f06f73b0d2d1af1487170fff2a60c20
Adi Eyal <adi@burgercom.co.za>
2013-10-12 16:30:51 +0200

    Alpha version works

--- server/mpr/urls.py
+++ server/mpr/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns("",
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="home"),
     url(r"^api/$", "mpr.views.api", name="api"),
     url(r"^admin/", include(admin.site.urls)),
 )
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- server/mpr/views.py
+++ server/mpr/views.py
@@ -2,11 +2,42 @@ import json
 from django.http import HttpResponse, Http404
 from mpr import models
 
+def as_currency(x):
+    x = float(x)
+    return "R %.2f" % x
+
+dosage_form = {
+    "Liq" : "liquid",
+    "Tab" : "tablet",
+    "Cap" : "capsule",
+    "Oin" : "ointment",
+    "Lit" : "lotion",
+    "Inj" : "injection",
+    "Syr" : "syrup",
+    "Eft" : "Effervescent",
+    "Drp" : "Drops",
+    "Sus" : "Suspension",
+    "Cal" : "Calasthetic",
+    "Sol" : "Solution",
+    "Neb" : "Nebuliser",
+    "Inh" : "Inhaler",
+    "Inf" : "Infusion",
+}
+
+def int_or_float(x):
+    try:
+        x = float(x)
+        if int(x) == float(x):
+            return int(x)
+        return x
+    except (ValueError, TypeError):
+        return "-"
+
 def serialize_ingredient(ingredient, strength):
     return {
         "name" : ingredient.name,
         "unit" : ingredient.unit,
-        "strength" : strength,
+        "strength" : int_or_float(strength),
     }
 
 def serialize_product(product):
@@ -14,10 +45,10 @@ def serialize_product(product):
         "regno" : product.regno,
         "schedule" : product.schedule,
         "name" : product.name,
-        "dosage_form" : product.dosage_form,
+        "dosage_form" : dosage_form.get(product.dosage_form, product.dosage_form),
         "pack_size" : product.pack_size,
         "num_packs" : product.num_packs,
-        "sep" : product.sep,
+        "sep" : as_currency(product.sep),
         "is_generic" : product.is_generic,
         "ingredients" : [
             serialize_ingredient(pi.ingredient, pi.strength)
@@ -30,15 +61,17 @@ def api(request):
     ingredsearch = request.GET.get("ingredient", "").strip()
 
     if len(ingredsearch) < 3:
-        raise Http404  
+        products = []
+    else:
+        ingredients = models.Ingredient.objects.filter(name__icontains=ingredsearch)
 
-    ingredients = models.Ingredient.objects.filter(name__icontains=ingredsearch)
+        products = set()
+        for i in ingredients:
+            products |= set(i.product_set.all())
 
-    products = set()
-    for i in ingredients:
-        products |= set(i.product_set.all())
+        products = sorted(products, key=lambda x: x.sep)
+        products = [serialize_product(p) for p in products]
 
-    products = [serialize_product(p) for p in products]
     return HttpResponse(
         json.dumps(products, indent=4), mimetype="application/json"
     )
