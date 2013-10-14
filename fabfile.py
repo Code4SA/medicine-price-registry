61de34258d5dd746df15e937e77859e5f69bd940
Adi Eyal <adi@burgercom.co.za>
2013-10-14 20:18:52 +0200

    updated fabfile to use new requirements directory

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ pip = "%s/bin/pip" % env_dir
 def deploy():
     with api.cd(code_dir):
         api.run("git pull origin master")
-        api.run("%s install -r %s/deploy/production.txt --quiet" % (pip, code_dir))
+        api.run("%s install -r %s/requirements/production.txt --quiet" % (pip, code_dir))
 
         with api.cd(os.path.join(code_dir, "server")):
             api.run("%s manage.py collectstatic --noinput --settings=settings.production" % python)
