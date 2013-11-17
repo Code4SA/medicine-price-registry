e021884ad9ccb729740bf90a025f8226801aaad8
Adi Eyal <adi@burgercom.co.za>
2013-11-17 17:06:28 +0200

    Added local git push to deployment

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,13 @@
 from fabdefs import *
 from fabric import api
+from fabric.operations import local
 import os
 
 python = "%s/bin/python" % env_dir
 pip = "%s/bin/pip" % env_dir
 
 def deploy():
+    local("git push origin master")
     with api.cd(code_dir):
         api.run("git pull origin master")
         api.run("%s install -r %s/requirements/production.txt --quiet" % (pip, code_dir))
