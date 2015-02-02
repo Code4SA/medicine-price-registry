b2f0a393fab094807a9310f1f54c97ac6e5fdc1e
Adi Eyal <adi@burgercom.co.za>
2015-02-02 10:13:15 +0200

    Changed sessions to cookies instead of DB
    Sqlite databases don't work on heroku if you write to them since the disk is emphemeral

--- mpr/settings.py
+++ mpr/settings.py
@@ -96,7 +96,8 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     "django.middleware.cache.UpdateCacheMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
+    #"django.contrib.sessions.middleware.SessionMiddleware",
+    'django.contrib.sessions.backends.signed_cookies',
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
