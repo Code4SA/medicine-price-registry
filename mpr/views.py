cdf51772c2d1b9c3c8f6792bb08f8cb10cf19519
Adi Eyal <adi@code4sa.org>
2017-01-07 20:26:42 +0200

    Storing UID in cookie instead of using a session cookie

--- mpr/views.py
+++ mpr/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, Http404
 from django.conf import settings
 import names
+import datetime
 
 from mpr import models
 import serialisers
@@ -10,7 +11,19 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def log_analytics(request, event, properties):
+def set_cookie(response, key, value, days_expire = 7):
+    max_age = 365 * 24 * 60 * 60  * 100
+    expires = datetime.datetime.strftime(
+        datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+    response.set_cookie(
+        key, value,
+        max_age=max_age,
+        expires=expires,
+        domain=settings.SESSION_COOKIE_DOMAIN,
+        secure=settings.SESSION_COOKIE_SECURE or None
+    )
+
+def log_analytics(request, response, event, properties):
     try:
         import analytics
         from ipware.ip import get_ip as get_ip
@@ -28,12 +41,13 @@ def log_analytics(request, event, properties):
 
         ip = get_ip(request)
 
-        name = names.get_full_name()
-        uid = request.session.get("uid", name)
+        uid = names.get_full_name()
+        uid = request.COOKIES.get("uid", uid)
+        set_cookie(response, "uid", uid)
+
         request.session["uid"] = uid
         analytics.init(api_key)
-        analytics.identify(uid,
-            {
+        analytics.identify(uid, {
                 "$name" : uid,
             },
             { "$ip" : ip}
@@ -70,10 +84,6 @@ def search_by_product(request):
 def search(request, serialiser=serialisers.serialize_products):
     q = request.GET.get("q", "").strip()
 
-    log_analytics(request, "#search", {
-        "search_string" : q
-    })
-
     all_products = set()
     if len(q) < 3:
         products = []
@@ -83,9 +93,17 @@ def search(request, serialiser=serialisers.serialize_products):
         all_products |= products1 | products2
         all_products = sorted(all_products, key=lambda x: x.sep)
         products = serialiser(all_products)
-    return HttpResponse(
+
+    response = HttpResponse(
         json.dumps(products, indent=4), mimetype="application/json"
     )
+
+    log_analytics(request, response, "#search", {
+        "search_string" : q
+    })
+
+    return response
+
 
 def search_lite(request):
     return search(request, serialisers.serialize_products_lite)
@@ -93,13 +111,15 @@ def search_lite(request):
 def related_products(request):
     product_id = request.GET.get("product", "").strip()
     product = get_object_or_404(models.Product, id=product_id)
-    log_analytics(request, "#related", product_properties(product))
 
-    return HttpResponse(
+    response = HttpResponse(
         json.dumps(
             serialisers.serialize_products(product.related_products), indent=4
         ), mimetype="application/json"
     )
+
+    log_analytics(request, response, "#related", product_properties(product))
+    return response
     
 def product_properties(product):
     return {
@@ -113,20 +133,21 @@ def product_detail(request):
     product_id = request.GET.get("product", "").strip()
     product = get_object_or_404(models.Product, id=product_id)
 
-    log_analytics(request, "#product-detail", product_properties(product))
-
-    return HttpResponse(
+    response = HttpResponse(
         json.dumps(
             serialisers.serialize_product(product), indent=4
         ), mimetype="application/json"
     )
 
+    log_analytics(request, response, "#product-detail", product_properties(product))
+    return response
+
 def dump(request):
-    log_analytics(request, "#dump", {})
 
-
-    return HttpResponse(
+    response = HttpResponse(
         json.dumps(
             serialisers.serialize_products(models.Product.objects.all()), indent=4
         ), mimetype="application/json"
     )
+    log_analytics(request, response, "#dump", {})
+    return response
