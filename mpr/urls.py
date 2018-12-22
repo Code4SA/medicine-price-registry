991a53912c3c211cefc84c0e89d1f7eb4f919ff2
Adi Eyal <adi@openup.org.za>
2018-12-22 13:49:44 +0200

    Preparing for django upgrade
    Updated greenlet and gevent
    Also providing callables instead of strings for urls.py

--- mpr/urls.py
+++ mpr/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 from mpr.models import LastUpdated
 
 from django.contrib import admin
+import apiv1, apiv2
 admin.autodiscover()
 
 class IndexView(TemplateView):
@@ -19,35 +20,35 @@ class IndexView(TemplateView):
             context['last_updated'] = u"Never"
         return context
 
-urlpatterns = patterns("",
+urlpatterns = [
     url(r'^$', IndexView.as_view(template_name="index.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
 
-    url(r"^api/related$", "mpr.apiv1.related_products", name="api_related_products"),
-    url(r"^api/detail$", "mpr.apiv1.product_detail", name="api_product_detail"),
-    url(r"^api/search/by_product$", "mpr.apiv1.search_by_product", name="api_search_by_product"),
-    url(r"^api/search/by_ingredient$", "mpr.apiv1.search_by_ingredient", name="api_search_by_ingredient"),
-    url(r"^api/search$", "mpr.apiv1.search", name="api_search"),
-    url(r"^api/search-lite$", "mpr.apiv1.search_lite", name="api_search_lite"),
-    url(r"^api/dump$", "mpr.apiv1.dump", name="api_dump"),
-    url(r"^api/v1/related$", "mpr.apiv1.related_products", name="api_related_products"),
-    url(r"^api/v1/detail$", "mpr.apiv1.product_detail", name="api_product_detail"),
-    url(r"^api/v1/search/by_product$", "mpr.apiv1.search_by_product", name="api_search_by_product"),
-    url(r"^api/v1/search/by_ingredient$", "mpr.apiv1.search_by_ingredient", name="api_search_by_ingredient"),
-    url(r"^api/v1/search$", "mpr.apiv1.search", name="api_search"),
-    url(r"^api/v1/search-lite$", "mpr.apiv1.search_lite", name="api_search_lite"),
-    url(r"^api/v1/dump$", "mpr.apiv1.dump", name="api_dump"),
+    url(r"^api/related$", apiv1.related_products, name="api_related_products"),
+    url(r"^api/detail$", apiv1.product_detail, name="api_product_detail"),
+    url(r"^api/search/by_product$", apiv1.search_by_product, name="api_search_by_product"),
+    url(r"^api/search/by_ingredient$", apiv1.search_by_ingredient, name="api_search_by_ingredient"),
+    url(r"^api/search$", apiv1.search, name="api_search"),
+    url(r"^api/search-lite$", apiv1.search_lite, name="api_search_lite"),
+    url(r"^api/dump$", apiv1.dump, name="api_dump"),
+    url(r"^api/v1/related$", apiv1.related_products, name="api_related_products"),
+    url(r"^api/v1/detail$", apiv1.product_detail, name="api_product_detail"),
+    url(r"^api/v1/search/by_product$", apiv1.search_by_product, name="api_search_by_product"),
+    url(r"^api/v1/search/by_ingredient$", apiv1.search_by_ingredient, name="api_search_by_ingredient"),
+    url(r"^api/v1/search$", apiv1.search, name="api_search"),
+    url(r"^api/v1/search-lite$", apiv1.search_lite, name="api_search_lite"),
+    url(r"^api/v1/dump$", apiv1.dump, name="api_dump"),
 
-    url(r"^api/v2/related$", "mpr.apiv2.related_products", name="api_related_products"),
-    url(r"^api/v2/detail$", "mpr.apiv2.product_detail", name="api_product_detail"),
-    url(r"^api/v2/search/by_product$", "mpr.apiv2.search_by_product", name="api_search_by_product"),
-    url(r"^api/v2/search$", "mpr.apiv2.search", name="api_search"),
-    url(r"^api/v2/search-lite$", "mpr.apiv2.search_lite", name="api_search_lite"),
-    url(r"^api/v2/last-updated$", "mpr.apiv2.last_updated", name="api_last_updated"),
+    url(r"^api/v2/related$", apiv2.related_products, name="api_related_products"),
+    url(r"^api/v2/detail$", apiv2.product_detail, name="api_product_detail"),
+    url(r"^api/v2/search/by_product$", apiv2.search_by_product, name="api_search_by_product"),
+    url(r"^api/v2/search$", apiv2.search, name="api_search"),
+    url(r"^api/v2/search-lite$", apiv2.search_lite, name="api_search_lite"),
+    url(r"^api/v2/last-updated$", apiv2.last_updated, name="api_last_updated"),
 
     # currently still using v1 methods since there hasn't been a change
-    url(r"^api/v2/dump$", "mpr.apiv1.dump", name="api_dump"),
-    url(r"^api/v2/search/by_ingredient$", "mpr.apiv1.search_by_ingredient", name="api_search_by_ingredient"),
-)
+    url(r"^api/v2/dump$", apiv1.dump, name="api_dump"),
+    url(r"^api/v2/search/by_ingredient$", apiv1.search_by_ingredient, name="api_search_by_ingredient"),
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
