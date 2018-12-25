57596780d0b72ab2d9c346564fa4335babf45d6e
Adi Eyal <adi@openup.org.za>
2018-12-25 11:11:25 +0200

    Upgraded to django 1.7

---
+++ mpr/test_all.py
@@ -0,0 +1,5 @@
+from tests.test_apiv1 import *
+from tests.test_apiv2 import *
+from tests.test_models import *
+from tests.test_views import *
+from tests.test_serialisers import *
--- mpr/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from test_apiv1 import *
-from test_apiv2 import *
-from test_models import *
-from test_views import *
-from test_serialisers import *
