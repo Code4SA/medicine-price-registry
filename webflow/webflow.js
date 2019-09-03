78474a8ac7861eb1ff0b259d94b34e95ea2805de
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-09-03 12:43:15 +0200

    add script for webflow mpr

--- webflow/webflow.js
+++ webflow/webflow.js
@@ -1,1 +1,139 @@
 /* webflow code */
+
+$(document).ready(function() {
+  $("#medicine-search").on("keyup", function(e) {
+    entermedicine(e);
+  });
+  $(function() {
+    // Bind the event.
+    $(window).on('hashchange', function() {
+      load_medicines(location.hash);
+    })
+    // Trigger the event (useful on page load).
+    load_medicines(location.hash);
+  });
+});
+
+var timer, delay = 500;
+var search_url = function(term) { return 'https://mpr.code4sa.org/api/v2/search-lite?q=' + term; }
+var related_url = function(id) { return 'https://mpr.code4sa.org/api/v2/related?nappi=' + id };
+var product_detail_url = function(id) { return 'https://mpr.code4sa.org/api/v3/detail?nappi=' + id };
+
+
+var entermedicine = function(e) {
+    searchTerm = e.target.value;
+    if (searchTerm.length < 4) return;
+    var load_data = function() {
+        location.hash = 'search:' + searchTerm;
+    }
+    var reset_delay_before_requesting = function() {
+        clearTimeout(timer);
+        timer = setTimeout(load_data, delay);
+    }
+    reset_delay_before_requesting();
+};
+var load_medicines = function(value) {
+    if (value.indexOf(':') >= 0) {
+        var s = value.split(':')
+        var key = s[0]
+        var value = s[1]
+        if (key == '#related') {
+            load_data(related_url(value), process_request);
+            log_analytics("product-related", {"id" : value});
+        } else if (key == '#search') {
+            load_data(search_url(value), process_request);
+        }
+    }
+}
+var process_request = function(result) {
+  $('.listing').hide();
+  $(".search-results").css("display", "block")
+  if (result.length > 0) {
+  	for (var i = 0; i < result.length; i++) {
+    	var datum = result[i];
+      var res = $(".listing")[i]
+      res = $(res)
+      if (res != undefined) {
+        $('.cc-listing-name', res).text(datum.name);
+        $('.listing-price', res).text(datum.sep);  
+        $('.show-more', res).data('data-nappi', datum.nappi_code);     
+        res.show();
+        $('.show-more', res).one('click', createClickCallBack(res));
+       }
+    }
+  }
+}
+
+function createClickCallBack(res) {
+  return function() {
+    var id = $(this).data('data-nappi');
+    load_data(product_detail_url(id), function(resultObject) {
+      return process_request_for_details(resultObject, res)
+    });
+  }
+}
+
+var process_request_for_details = function(resultObject, listing) {
+  if (resultObject) {
+    var res = $('.listing-accordion-content', listing);
+    res = $(res)
+    if (res != undefined) {
+      $('.single-exit-price', res).text(resultObject.sep);
+      $('.max-dispensing-fee', res).text(resultObject.dispensing_fee);
+      $('.cost-per-capsule', res).text(`${resultObject.min_price} - ${resultObject.max_price}`);
+      $('.schedule', res).text(resultObject.schedule);
+      $('.dosage-form', res).text(resultObject.dosage_form);
+      $('.tablets-doses', res).text(resultObject.pack_size);
+      $('.number-packs', res).text(resultObject.num_packs);
+      $('.generic', res).text(resultObject.is_generic);
+      $('.registration', res).text(`Registration Number: ${resultObject.regno}`);
+      var ingredientsWrapper = $('.ingredients-wrapper', res);
+      var $ingredient = $('.ingredient', res);
+      $ingredient.hide();
+      var ingredientsArray = resultObject.ingredients
+      if (ingredientsArray) {
+        for(var i = 0; i < ingredientsArray.length; i++) {
+          var clone = $ingredient.clone();
+          clone.show();
+
+          var $product = new Product(ingredientsArray[i], clone);
+            
+          ingredientsWrapper.append($product.build_product());
+
+
+          clone.find('.row-title').text(ingredientsArray[i].name)
+          clone.find('.row-value').text(`${ingredientsArray[i].strength}${ingredientsArray[i].unit}`);
+        }
+      }
+    }
+  }
+}
+
+var load_data = function(url, foo) {
+    return $.getJSON(url, function(data) {
+        foo(data);
+    });
+}
+
+Product = function(data, block) { 
+  this.data = data;
+  this.block = block;
+}
+
+Product.prototype = {
+
+  set_title : function() {
+      $('.row-title', this.block).html('href', '#product-detail-' + this.data.name);
+  },
+
+  set_value : function() {
+      $('.row-value', this.block).html(`${this.data.strength}${this.data.unit}`);
+  },
+
+  build_product : function() {
+      this.set_title();
+      this.set_value();
+
+      return this.block;
+  }
+}
