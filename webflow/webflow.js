ef8546e09e285e66a799d97063d32ebca72fc2c6
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-04 15:09:49 +0200

    add updated script for webflow demo site

--- webflow/webflow.js
+++ webflow/webflow.js
@@ -48,7 +48,7 @@ var load_medicines = function(value) {
         var key = s[0]
         var value = s[1]
         if (key == '#related') {
-            load_data(related_url(value), process_request);
+            load_data(related_url(value), process_request_for_generics);
         } else if (key == '#search') {
             load_data(search_url(value), process_request);
         }
@@ -74,7 +74,7 @@ var process_request = function(result) {
       res.show();
       
       $('.generics-link', res).click(createClickGenericCallBack(res));
-      $('.listing-accordion-trigger', res).one('click', createClickCallBack(res));
+      $('.listing-accordion-trigger', res).click(createClickCallBack(res, datum.nappi_code));
       }
   }
 }
@@ -82,14 +82,27 @@ var process_request = function(result) {
 function createClickGenericCallBack(res) {
   return function(event) {
     event.stopImmediatePropagation();
+    event.preventDefault();
     var id = $(this).data('data-id');
     load_data(related_url(id), process_request_for_generics, res);
+    location.hash = '#related:' + id;
   }
 }
 
-function createClickCallBack(res) {
+function createClickCallBack(res, nappiCode) {
   return function() {
-    var id = $(this).data('data-nappi');
+    $(this).data('data-nappi', nappiCode);
+    var id =  $(this).data('data-nappi');
+    load_data(product_detail_url(id), function(resultObject) {
+      return process_request_for_details(resultObject, res)
+    });
+  }
+}
+
+function createClickCallBackTwo(res, nappiCode) {
+  return function() {
+    $(this).data('data-nappi', nappiCode);
+    var id =  $(this).data('data-nappi');
     load_data(product_detail_url(id), function(resultObject) {
       return process_request_for_details(resultObject, res)
     });
@@ -104,6 +117,8 @@ var process_request_for_generics = function(result, listing) {
   $('.show-more > img').css({'transform': 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
     'transform-style': 'preserve-3d'});
   $('.listing-accordion-content').css({'display': 'none'});
+  $('.home-title-wrapper').css({'margin-top': 0, 'height': 0, 'opacity': 0, 'transition': 'height 0.1s ease-out, opacity 0.1s ease-out, margin-top 0.1s ease-out'});
+  $('.hero-image').css({'height': 0, 'opacity': 0, 'transition': 'height 0.1s ease-out, opacity 0.1s ease-out'});
   if (result.length > 0) {
     $('#results-number', res).text(result.length);
   	for (var i = 0; i < result.length; i++) {
@@ -113,8 +128,9 @@ var process_request_for_generics = function(result, listing) {
       if (res != undefined) {
         $('.cc-listing-name', res).text(datum.name);
         $('.listing-price', res).text(datum.sep);
-        $('.generics-link', res).data('data-id', datum.nappi_code);  
+        $('.generics-link', res).data('data-id', datum.nappi_code);
         res.show();
+        $('.listing-accordion-trigger', res).click(createClickCallBackTwo(res, datum.nappi_code));
        }
     }
   }
@@ -126,9 +142,15 @@ var process_request_for_details = function(resultObject, listing) {
     var res = $('.listing-accordion-content', listing);
     res = $(res);
     if (res != undefined) {
+      if(resultObject.dosage_form !== "tablet" && resultObject.dosage_form !== "capsule") {
+        $('.cost-per-unit-wrapper', res).remove();
+      }
       $('.single-exit-price', res).text(resultObject.sep);
       $('.max-dispensing-fee', res).text(resultObject.dispensing_fee);
-      $('.cost-per-capsule', res).text(`${resultObject.min_price} - ${resultObject.max_price}`);
+      $('.price-range', res).text(`${resultObject.min_price} - ${resultObject.max_price}`);
+      if( $('.cost-per-unit', res)) {
+        $('.cost-per-unit', res).text(`${resultObject.min_cost_per_unit} / ${resultObject.dosage_form} - ${resultObject.max_cost_per_unit} / ${resultObject.dosage_form}`);
+      }
       $('.schedule', res).text(resultObject.schedule);
       $('.dosage-form', res).text(resultObject.dosage_form);
       $('.tablets-doses', res).text(resultObject.pack_size);
@@ -138,16 +160,16 @@ var process_request_for_details = function(resultObject, listing) {
       var ingredientsWrapper = $('.ingredients-wrapper', res);
       var $ingredient = $('.ingredient', res);
       $ingredient.hide();
-      var ingredientsArray = resultObject.ingredients
+      var ingredientsArray = resultObject.ingredients;
       if (ingredientsArray) {
         for(var i = 0; i < ingredientsArray.length; i++) {
           var clone = $ingredient.clone();
           clone.show();
 
           var $product = new Product(ingredientsArray[i], clone);
-
-            $('.ingredients-wrapper div:nth-child(2)', res).remove();
-            ingredientsWrapper.append($product.build_product());
+          
+          $('.ingredients-wrapper div:nth-child(2)', res).remove();
+          ingredientsWrapper.append($product.build_product());
 
 
           clone.find('.row-title').text(ingredientsArray[i].name)
